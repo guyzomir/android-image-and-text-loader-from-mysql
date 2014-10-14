9f9ee4bdfddcd56a644e3463c921a0607f6f204a
adminCOE <admincoe@ltbulfinmi01.lewisu.local>
2014-10-14 15:29:27 -0500

    code

---
+++ app/src/main/java/com/guy/imageloader/MyActivity.java
@@ -0,0 +1,118 @@
+package com.guy.imageloader;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+public class MyActivity extends Activity {
+
+    // Declare Variables
+    JSONObject jsonobject;
+    JSONArray jsonarray;
+    ListView listview;
+    ListViewAdapter adapter;
+    ProgressDialog mProgressDialog;
+    ArrayList<HashMap<String, String>> arraylist;
+    static String RANK = "Event_name";
+    static String COUNTRY = "Event_location";
+    static String POPULATION = "time";
+    static String FLAG = "link_image";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        setContentView(R.layout.listview_main);
+        // Execute DownloadJSON AsyncTask
+        new DownloadJSON().execute();
+
+    }
+
+    private class DownloadJSON extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Create a progressdialog
+            mProgressDialog = new ProgressDialog(MyActivity.this);
+            // Set progressdialog title
+            mProgressDialog.setTitle("Android JSON Parse Tutorial");
+            // Set progressdialog message
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(false);
+            // Show progressdialog
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            // Create an array
+            arraylist = new ArrayList<HashMap<String, String>>();
+            // Retrieve JSON Objects from the given URL address
+            jsonobject = JSONfunctions
+                    .getJSONfromURL("http://uscameroonchamber.org/yamo.php");
+
+            try {
+                // Locate the array name in JSON
+                jsonarray = jsonobject.getJSONArray("events");
+
+                for (int i = 0; i < jsonarray.length(); i++) {
+                    HashMap<String, String> map = new HashMap<String, String>();
+                    jsonobject = jsonarray.getJSONObject(i);
+                    // Retrive JSON Objects
+                    map.put("Event_name", jsonobject.getString("Event_name"));
+                    map.put("Event_location", jsonobject.getString("Event_location"));
+                    map.put("time", jsonobject.getString("time"));
+                    map.put("link_image", jsonobject.getString("link_image"));
+                    // Set the JSON Objects into the array
+                    arraylist.add(map);
+                }
+            } catch (JSONException e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void args) {
+            // Locate the listview in listview_main.xml
+            listview = (ListView) findViewById(R.id.listview);
+            // Pass the results into ListViewAdapter.java
+            adapter = new ListViewAdapter(MyActivity.this, arraylist);
+            // Set the adapter to the ListView
+            listview.setAdapter(adapter);
+            // Close the progressdialog
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
